bb9b9ee1df41930d77c38dce6c916642d7b341b5
Harsh Pandya <hvpandya@ncsu.edu>
2015-03-04 17:45:22 -0500

    updated test.cpp and depth.cpp

--- src/depth/src/prime.cpp
+++ src/depth/src/prime.cpp
@@ -5,16 +5,12 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <string.h>
-#include <ros/callback_queue.h>
-
 //#include <ifs.h>
 
 using namespace std;
 //using namespace cv;
-char filename[50];
-int imgtype;
+char filename[100];
 
-bool done=false;
 
 static const int MY_ROS_QUEUE_SIZE = 100;
 void depth_node_callback(const sensor_msgs::Image::ConstPtr& msg)
@@ -22,7 +18,8 @@ void depth_node_callback(const sensor_msgs::Image::ConstPtr& msg)
 // The message's data is a raw buffer. While the type is uint8_t*, the
 // data itself is an array of floats (for depth data), the value of
 // the float being the distance in meters.
-
+cout << "Top-left corner: " << *reinterpret_cast<const float*>(&msg->data[0]) << "m" << endl;
+try {
 cv_bridge::CvImageConstPtr cv_ptr;
 cv_ptr = cv_bridge::toCvShare(msg);
 // imshow expects a float value to lie in [0,1], so we need to normalize
@@ -33,33 +30,29 @@ cv::Mat normalized,op;
 cv_ptr->image.convertTo(normalized, CV_32F, 1.0/max, 0) ;
 cv_ptr->image.convertTo(op, CV_8U, 255/max, 0) ;
 
+cv::imshow("depth_node", normalized);
 cv::imwrite(filename,op);
-cout<<"Been there done that"<<endl;
-done=true;
+cv::waitKey(1);
+} catch (const cv_bridge::Exception& e) {
+ROS_ERROR("cv_bridge exception: %s", e.what());
+}
 }
 
 int main(int argc, char **argv)
 {
-string type;
+
 strcpy(filename,argv[1]);
-imgtype=atoi(argv[2]);
 
 
-ros::init(argc, argv, "depth_node");
+ros::init(argc, argv, "prime_node");
 
-cout << "Ready to save image" << endl;
+cout << "Ready to stream" << endl;
 ros::NodeHandle nh;
+ros::Subscriber sub = nh.subscribe("camera/rgb/image_color",MY_ROS_QUEUE_SIZE, depth_node_callback);
 
-if(imgtype==1)
-type="camera/rgb/image_color";
-
-else if(imgtype==0)
-type="camera/depth/image_raw";
-
-ros::Subscriber sub = nh.subscribe(type,MY_ROS_QUEUE_SIZE, depth_node_callback);
-
-
-while(!done) ros::spinOnce();
-cout <<filename<<" Saved" << endl;
+cv::namedWindow("prime_node");
+ros::spin();
+cv::destroyWindow("prime_node");
+cout << "Stream Ended" << endl;
 return 0;
 }
